b1ca1038e83d197f616640fd2e981af8ee7f6c71
SahilSonar <sss.sonar2003@gmail.com>
2020-09-17 11:59:58 +0530

    [TMP] sdm660-common: Switch to minimal sepolicy
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -161,7 +161,7 @@ BOARD_USES_QC_TIME_SERVICES := true
 PRIVATE_EXCLUDE_BUILD_TEST := true
 
 # Sepolicy
-include device/qcom/sepolicy-legacy-um/sepolicy.mk
+#include device/qcom/sepolicy-legacy-um/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 
 # Vendor Security Patch Level
